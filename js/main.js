92805351eb0b804fcfd54142bbb55fe59cbb4710
Go Tanaka <tanaka@plugram.co.jp>
2014-11-02 13:13:49 +0900

    added contents

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+$(function(){
+    //$('#playground').hide();
 
+    $('.p5').click(function(event){
+        event.preventDefault();
+
+        $('playground').innerHTML = '<b>hello</b>';
+        $('playground').show();
+    });
+});
